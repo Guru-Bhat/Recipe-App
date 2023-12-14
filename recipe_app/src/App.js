b0f966e026f8353c42042ad53a37f5d04dbd1b2b
Guru-Bhat <mailto:gurubhat5198@gmail.com>
2023-12-14 21:52:33 +0530

    initial setup

---
+++ recipe_app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Routes from './Routes/Routes'
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
